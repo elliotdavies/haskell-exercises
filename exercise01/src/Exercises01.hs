f955ea6b6ef5f34d1f798b7ece51e56414e9735d
Elliot Davies <elliot.a.davies@gmail.com>
2018-11-08 23:46:52 +0000

    Almost done with GADTs

--- exercise01/src/Exercises01.hs
+++ exercise01/src/Exercises01.hs
@@ -31,6 +31,7 @@ countList (CCons head tail) = count head + countList tail
 -- | c. Write a function that removes all elements whose count is 0.
 
 dropZero :: CountableList -> CountableList
+dropZero CNil = CNil
 dropZero (CCons head tail) =
   if count head == 0 then
     dropZero tail
@@ -44,6 +45,7 @@ dropZero (CCons head tail) =
 filterInts :: CountableList -> CountableList
 filterInts = error "Contemplate me!"
 
+--    No, because all we know is that the items are Countable, not what types they are
 
 
 
@@ -63,13 +65,15 @@ reverseAnyList ANil = ANil
 reverseAnyList (ACons head tail) =
   ACons (reverseAnyList tail) (ACons head ANil) -- Same idea as `reverse xs ++ [x]`
 
+-- Technically you can do this for (\_ -> True) or similar but not for any useful predicate
 filterAnyList :: (a -> Bool) -> AnyList -> AnyList
-filterAnyList = undefined
+filterAnyList = undefined 
 
 countAnyList :: AnyList -> Int
 countAnyList ANil = 0
 countAnyList (ACons _ tail) = 1 + countAnyList tail
 
+-- Again, technically you could constantly fold `mempty` but you can't write this usefully
 foldAnyList :: Monoid m => AnyList -> m
 foldAnyList = undefined
 
@@ -77,6 +81,7 @@ isEmptyAnyList :: AnyList -> Bool
 isEmptyAnyList ANil = True
 isEmptyAnyList _    = False
 
+-- The items aren't showable, so no
 instance Show AnyList where
   show = error "What about me?"
 
@@ -173,18 +178,21 @@ getInt (IntBox int _) = int
 -- pattern-match:
 
 getInt' :: MysteryBox String -> Int
-getInt' _doSomeCleverPatternMatching = error "Return that value"
+getInt' (StringBox s (IntBox i _)) = i 
 
 -- | b. Write the following function. Again, don't overthink it!
 
 countLayers :: MysteryBox a -> Int
-countLayers = error "Implement me"
+countLayers EmptyBox = 1
+countLayers (IntBox _ inner) = 1 + countLayers inner
+countLayers (StringBox _ inner) = 1 + countLayers inner
+countLayers (BoolBox _ inner) = 1 + countLayers inner
 
 -- | c. Try to implement a function that removes one layer of "Box". For
 -- example, this should turn a BoolBox into a StringBox, and so on. What gets
 -- in our way? What would its type be?
 
-
+--    The return type would need to be different for each input box type
 
 
 
@@ -205,17 +213,24 @@ exampleHList = HCons "Tom" (HCons 25 (HCons True HNil))
 -- need to pattern-match on HNil, and therefore the return type shouldn't be
 -- wrapped in a 'Maybe'!
 
+head' :: HList (head, tail) -> head
+head' (HCons h _) = h
+
 -- | b. Currently, the tuples are nested. Can you pattern-match on something of
 -- type @HList (Int, String, Bool, ())@? Which constructor would work?
 
 patternMatchMe :: HList (Int, String, Bool, ()) -> Int
 patternMatchMe = undefined
 
+--    No: there is no HList constructor that matches the type signature here
+
 -- | c. Can you write a function that appends one 'HList' to the end of
 -- another? What problems do you run into?
 
-
-
+--    Not without knowing the specific types. With unknown types there is no way to
+--    the tail in order to match the HList (head, tail) constructor. For example:
+--
+--      append :: HList (h1, t1) -> HList (h2, t2) -> HList (h1, (???))
 
 
 {- SEVEN -}
@@ -229,16 +244,24 @@ data Branch left centre right
 -- /tree/. None of the variables should be existential.
 
 data HTree a where
-  -- ...
+  HEmpty  :: HTree ()
+  HBranch :: HTree l -> HTree c -> HTree r -> HTree (l, c, r) 
 
 -- | b. Implement a function that deletes the left subtree. The type should be
 -- strong enough that GHC will do most of the work for you. Once you have it,
 -- try breaking the implementation - does it type-check? If not, why not?
 
+deleteLeft :: HTree (l, c, r) -> HTree ((), c, r)
+deleteLeft (HBranch l c r) = HBranch HEmpty c r
+
 -- | c. Implement 'Eq' for 'HTree's. Note that you might have to write more
 -- than one to cover all possible HTrees. Recursion is your friend here - you
 -- shouldn't need to add a constraint to the GADT!
 
+instance Eq (HTree a) where
+  HEmpty        == HEmpty           = True
+  HBranch l c r == HBranch l' c' r' = l == l' && c == c' && r == r'
+  _             == _                = False
 
 
 
@@ -254,24 +277,32 @@ data HTree a where
 -- @
 
 data AlternatingList a b where
-  -- ...
+  AltNil  :: AlternatingList a b
+  AltCons :: a -> b -> AlternatingList a b -> AlternatingList a b
+
 
 -- | b. Implement the following functions.
 
 getFirsts :: AlternatingList a b -> [a]
-getFirsts = error "Implement me!"
+getFirsts AltNil = []
+getFirsts (AltCons a b rest) = a : getFirsts rest
+
 
 getSeconds :: AlternatingList a b -> [b]
-getSeconds = error "Implement me, too!"
+getSeconds AltNil = []
+getSeconds (AltCons a b rest) = b : getSeconds rest
 
 -- | c. One more for luck: write this one using the above two functions, and
 -- then write it such that it only does a single pass over the list.
 
 foldValues :: (Monoid a, Monoid b) => AlternatingList a b -> (a, b)
-foldValues = error "Implement me, three!"
+foldValues list = (mconcat $ getFirsts list, mconcat $ getSeconds list)
 
-
-
+foldValues' :: (Monoid a, Monoid b) => AlternatingList a b -> (a, b)
+foldValues' = go (mempty, mempty)
+  where
+    go acc AltNil = acc
+    go (accA, accB) (AltCons a b rest) = go (accA <> a, accB <> b) rest
 
 
 {- NINE -}
@@ -290,7 +321,11 @@ data Expr a where
 -- | a. Implement the following function and marvel at the typechecker:
 
 eval :: Expr a -> a
-eval = error "Implement me"
+eval (Equals (IntValue i) (IntValue j)) = i == j
+eval (Add (IntValue i) (IntValue j))    = i + j
+eval (If (BoolValue cond) t f)          = if cond then eval t else eval f
+eval (IntValue i)                       = i
+eval (BoolValue b)                      = b
 
 -- | b. Here's an "untyped" expression language. Implement a parser from this
 -- into our well-typed language. Note that (until we cover higher-rank
@@ -304,11 +339,15 @@ data DirtyExpr
   | DirtyBoolValue Bool
 
 parse :: DirtyExpr -> Maybe (Expr Int)
-parse = error "Implement me"
+parse (DirtyIntValue i) = Just (IntValue i)
+parse (DirtyAdd dx dy) = Add <$> parse dx <*> parse dy
+parse (DirtyIf (DirtyBoolValue cond) dx dy)
+  = If (BoolValue cond) <$> parse dx <*> parse dy
+-- Can't parse DirtyEquals or DirtyBool since they return Bool and not Int
+parse _ = Nothing
 
 -- | c. Can we add functions to our 'Expr' language? If not, why not? What
 -- other constructs would we need to add? Could we still avoid 'Maybe'?
-
 
 
 
@@ -324,13 +363,15 @@ parse = error "Implement me"
 -- long as the input of one lines up with the output of the next.
 
 data TypeAlignedList a b where
-  -- ...
+  TNil  :: TypeAlignedList a a
+  TCons :: (b -> c) -> TypeAlignedList a b -> TypeAlignedList a c
 
 -- | b. Which types are existential?
+
+--    Only `c`
 
 -- | c. Write a function to append type-aligned lists. This is almost certainly
 -- not as difficult as you'd initially think.
 
 composeTALs :: TypeAlignedList b c -> TypeAlignedList a b -> TypeAlignedList a c
-composeTALs = error "Implement me, and then celebrate!"
-
+composeTALs = error "todo"
